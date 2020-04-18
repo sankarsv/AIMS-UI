1976d86483d27adbc53764e3ce3ea45f81da31c4
reegan <reegancse78@gmail.com>
2020-04-18 05:09:56 -0400

    employee payload

--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -13,14 +13,22 @@ export class EmployeeService {
   constructor(private http:HttpClient) { }
 
   getEmployeeDetails(key, value):Promise<EmployeeDetails[]> {
-    let params = new HttpParams();
-    params = params.append(key, value);
-    return this.http.post(APP_CONSTANTS.URL[environment.type].SEARCH, params).toPromise().then(
+    return this.http.post(environment.baseUrl + APP_CONSTANTS.URL[environment.type].SEARCH, this.getPayLoad(key, value)).toPromise().then(
       (result:any) => {
              return result;
       },
       err => {
         throw err;        
       });
+  }
+
+  getPayLoad(key, value) {
+    if(key === 'empId') {
+      return {'empId': value};
+    } else if(key === 'dmId') {
+      return {'dmId': value};
+    } else if(key === 'glId') {
+      return {'glId': value};
+    }
   }
 }
